58eacea1dfa9b7e43e4de0bd4834dc596002298d
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-03-15 12:03:12 +0100

    Update les_documents_confidentiels.sh

--- TerminusQuest/Campus/Bethanie/A22/Amphi_Henri_Poincaré/les_documents_confidentiels.sh
+++ TerminusQuest/Campus/Bethanie/A22/Amphi_Henri_Poincaré/les_documents_confidentiels.sh
@@ -3,3 +3,10 @@
 
 echo "Pour valider cette quête, tu dois te rendre au guicher unique en A22 et trouver
 ce qui t’appartient, une autre quête t’y attendra..."
+
+while  [${PWD##*/}=!guichet_unique]
+do
+read cdutil
+cd ${cdutil:2}
+done
+
