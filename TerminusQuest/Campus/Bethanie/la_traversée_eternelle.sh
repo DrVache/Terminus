03767388f25a39391dcff318cc2bd750cafc0b3b
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-03-30 18:15:12 +0100

    Update la_traversée_eternelle.sh

--- TerminusQuest/Campus/Bethanie/la_traversée_eternelle.sh
+++ TerminusQuest/Campus/Bethanie/la_traversée_eternelle.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-
-
 echo " <Pr. Chen> : Rends-toi à l’Amphithéâtre Henri Poincaré dans le bâtiment
 A22, tu es en retard jeune dress... *hum hum* élève. Dépêche-toi."
+while [ true ]
+do
+	read cdutil
+case $cdutil in
+
+        "ls") #Rajouter l option -l ?
+                ls
+                ;;
+		   "INVENTAIRE")  #avec "$" devant comme dans le cahier des charges, rien ne se passe
+                echo "Hé Jean2, ça va ?"
+                ;;	
+	  	 "jobs")
+               jobs
+                ;;		
+        *)
+                if [ ${cdutil:0:2} = "cd" ]
+				        then
+					         cd ${cdutil:3}
+				        elif [ ${cdutil:0:3} = "cat" ]
+				        then
+					          cat ${cdutil:4}
+				        fi
+                ;;
+esac
+
+if [ ${PWD##*/} = "Amphi_Henri_Poincaré" ]
+		then
+		echo "fini"
+		exit
+	fi
+done
