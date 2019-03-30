14187b551ae6af1e8d9032a502e8371a387663e9
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-03-30 18:48:56 +0100

    Update le_rangement_IKEA.sh
    ATTENTION ! Changer le chemin en fonction de l'arborescence du projet

--- TerminusQuest/Campus/Bethanie/A22/guichet_unique/login/le_rangement_IKEA.sh
+++ TerminusQuest/Campus/Bethanie/A22/guichet_unique/login/le_rangement_IKEA.sh
@@ -2,3 +2,39 @@
 
 echo "Déplaces ces deux objets dans ton inventaire ! Puis va vérifier dans ton inventaire
 si tu as bien effectué le déplacement"
+while [ true ]
+do
+	read cdutil
+case $cdutil in
+
+        "ls") #Rajouter l option -l ?
+                ls
+                ;;
+		   "INVENTAIRE")  #avec "$" devant comme dans le cahier des charges, rien ne se passe
+                echo "Hé Jean2, ça va ?"
+                ;;	
+	  	 "jobs")
+               jobs
+                ;;		
+        *)
+                if [ ${cdutil:0:2} = "cd" ]	
+				        then
+					         cd ${cdutil:3}
+				        elif [ ${cdutil:0:3} = "cat" ]
+				        then
+					          cat ${cdutil:4}
+				        elif [ ${cdutil:0:2} = "mv" ]
+						then
+							mv ${cdutil:3}
+						fi
+                ;;
+esac
+
+
+#-------METTRE LE CHEMIN ABSOLUE DU PROJET !--------
+if [ -f "/mnt/c/TerminusQuest/Campus/Bethanie/A22/inventaire/carte_etudiante" ]	#-------METTRE LE CHEMIN ABSOLUE DU PROJET !--------
+	then
+		echo "fini"
+		exit
+	fi
+done
