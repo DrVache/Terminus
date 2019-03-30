255988b10b9f7df419843f145d3259b6ca3bcc39
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-03-30 17:44:11 +0100

    Update les_documents_confidentiels.sh

--- TerminusQuest/Campus/Bethanie/A22/Amphi_Henri_Poincaré/les_documents_confidentiels.sh
+++ TerminusQuest/Campus/Bethanie/A22/Amphi_Henri_Poincaré/les_documents_confidentiels.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 echo "Pour valider cette quête, tu dois te rendre au guicher unique en A22 et trouver
 ce qui t’appartient, une autre quête t’y attendra..."
+
 while [ true ]
 do
-read cdutil
-cd ${cdutil:3}
+	read cdutil
+case $cdutil in
 
-echo ${PWD##*/}
+        "ls")
+                ls 
+                ;;
+		 "INVENTAIRE")
+                echo "Voila l'inventaire (a compléter)"
+                ;;	
+		 "jobs")
+               jobs
+                ;;		
+        *)
+                if [ ${cdutil:0:2} = "cd" ]
+				then
+					cd ${cdutil:3}
+				elif [ ${cdutil:0:3} = "cat" ]
+				then
+					cat ${cdutil:4}
+				fi
+                ;;
+esac
+#echo ${PWD##*/}
 if [ ${PWD##*/} = "guichet_unique" ]
-then
-	echo "fini"
-	exit
-fi
-
+		then
+		echo "fini"
+		exit
+	fi
 done
