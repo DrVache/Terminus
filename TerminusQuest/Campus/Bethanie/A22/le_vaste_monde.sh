90c342df67f98eb6cb7c425b433ada38c65fafa0
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-04-01 18:36:55 +0200

    Update le_vaste_monde.sh

--- TerminusQuest/Campus/Bethanie/A22/le_vaste_monde.sh
+++ TerminusQuest/Campus/Bethanie/A22/le_vaste_monde.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
 echo "Réussiras-tu à résoudre l’énigme du Pr. Ofaite et à devenir omniscient ?"
+
+while [ true ]
+do
+	read cdutil
+case $cdutil in
+
+		"tree")
+				tree
+				echo "fini"
+				exit
+				;;
+        "ls") #Rajouter l option -l ?
+                ls
+                ;;
+		   "INVENTAIRE")  #avec "$" devant comme dans le cahier des charges, rien ne se passe
+                echo "Hé Jean2, ça va ?"
+                ;;	
+	  	 "jobs")
+               jobs
+                ;;		
+        *)
+                if [ ${cdutil:0:2} = "cd" ]	
+				        then
+					         cd ${cdutil:3}
+				        elif [ ${cdutil:0:3} = "cat" ]
+				        then
+					          cat ${cdutil:4}
+				        elif [ ${cdutil:0:2} = "mv" ]
+						then
+							mv ${cdutil:3}
+						fi
+                ;;
+esac
+done
