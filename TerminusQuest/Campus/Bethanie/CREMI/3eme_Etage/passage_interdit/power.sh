4fb48edd50d9d0ecbb542f23cc64cd24a3cb75ad
FlorianGardes <47028943+FlorianGardes@users.noreply.github.com>
2019-04-02 12:02:05 +0200

    Update power.sh

--- TerminusQuest/Campus/Bethanie/CREMI/3eme_Etage/passage_interdit/power.sh
+++ TerminusQuest/Campus/Bethanie/CREMI/3eme_Etage/passage_interdit/power.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Le but de cette quête est de piéger le joueur dans un endroit où on lui
-dit de ne aps se rendre. La commande yes est une boucle infinie, pour en sortir
+dit de ne pas se rendre. La commande yes est une boucle infinie, pour en sortir
 il faut utiliser CTRL + C."
 
 while [ true ]
