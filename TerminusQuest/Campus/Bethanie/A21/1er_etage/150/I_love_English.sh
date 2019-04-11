3e308c8d38ad46834ba4227286d4cfb1e93b7885
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-04-11 17:53:43 +0200

    Update I_love_English.sh

--- TerminusQuest/Campus/Bethanie/A21/1er_etage/150/I_love_English.sh
+++ TerminusQuest/Campus/Bethanie/A21/1er_etage/150/I_love_English.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
-echo "Trouve l’objet caché dans cette salle.
-Réponse juste :
-<Dora> : Oui c’est bien celui là ! We did it ! Hourra ! Hidden signifie caché !
-Réponse fausse :
-<Dora> : Non, je ne pense pas que ce soit ça, cherche mieux avant que Chipeur
-n’arrive nous le voler !"
+echo "Trouve l’objet caché dans cette salle."
+
+while [ true ]
+do
+
+	read cdutil
+case $cdutil in	
+
+		   "INVENTAIRE")  #avec "$" devant comme dans le cahier des charges, rien ne se passe
+                echo "Hé Jean2, ça va ?"
+                ;;	
+	  	 "jobs")
+               jobs
+                ;;
+				".hidden")
+					echo "Oui c'est bien celui là ! We did it ! Hourra ! Hidden signifie caché"	
+					exit
+					;;
+				"cat .hidden")
+					echo "Oui c'est bien celui là ! We did it ! Hourra ! Hidden signifie caché"	
+					exit
+					;;
+	*)
+					
+					   if [ ${cdutil:0:2} = "cd" ]
+				        then
+					         cd ${cdutil:3}
+				        elif [ ${cdutil:0:3} = "cat" ]
+				        then
+							cat ${cdutil:4}
+						elif [ ${cdutil:0:2} = "ls" ]
+				        then
+							ls ${cdutil:3}
+						elif [ ${cdutil:0:3} = "man" ]
+				        then
+							man ${cdutil:4}
+							fi
+esac
+
+
+done
