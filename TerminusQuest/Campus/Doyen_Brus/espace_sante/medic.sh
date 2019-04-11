791791c26947e077ad52d31b3a062fce705c0684
FlorianGardes <fl.gardes@yahoo.fr>
2019-04-11 16:36:55 +0200

    medic.sh à l'arrache

--- TerminusQuest/Campus/Doyen_Brus/espace_sante/medic.sh
+++ TerminusQuest/Campus/Doyen_Brus/espace_sante/medic.sh
@@ -1,13 +1,57 @@
 #!/bin/bash
 
-echo "<Mercy> : Tapes la troisième lettre de mon prénom.
-<Joueur> : R
-<Mercy> : Tape la lettre manquante : INFORM_TIQUE
-<Joueur> : A
-<Mercy> : Tape la 19ème lettre de l’alphabet.
-<Joueur> : S
-<Mercy> : Tout va très bien, tu peux récupérer ton certificat
-Si le joueur se trompe :
-<Mercy> : Désolé, mais tu n’as pas l’air en forme, réessaie plus tard.
-S’il réussie :
-<Mercy> : Bravo, voici ton certificat !"
+#echo "<Mercy> : Tapes la troisième lettre de mon prénom.
+#<Joueur> : R
+#<Mercy> : Tape la lettre manquante : INFORM_TIQUE
+#<Joueur> : A
+#<Mercy> : Tape la 19ème lettre de l’alphabet.
+#<Joueur> : S
+#<Mercy> : Tout va très bien, tu peux récupérer ton certificat
+#Si le joueur se trompe :
+#<Mercy> : Désolé, mais tu n’as pas l’air en forme, réessaie plus tard.
+#S’il réussie :
+#<Mercy> : Bravo, voici ton certificat !"
+
+echo "Bienvenue à l'infirmerie, nous allons voir comment tu te sens"
+echo "<Mercy> : Tapes la troisième lettre de mon prénom."
+
+while [ true ]
+ do
+  #statements
+  read cdutils
+case $cdutils in
+
+ "ls")
+ ls
+ ;;
+
+  "R")
+  echo "Tapes la lettre manquante : INFORM_TIQUE"
+  ;;
+  "r")
+  echo "Tapes la lettre manquante : INFORM_TIQUE"
+  ;;
+
+  "A")
+  echo "Tape la 19ème lettre de l’alphabet."
+  ;;
+  "a")
+  echo "Tape la 19ème lettre de l’alphabet."
+  ;;
+
+  "S")
+  echo "Tout va très bien, tu peux récupérer ton certificat"
+  exit
+  ;;
+  "s")
+  echo "Tout va très bien, tu peux récupérer ton certificat"
+  exit
+  ;;
+
+  *)
+  echo "Désolé, mais tu n’as pas l’air en forme, réessaie plus tard."
+  exit
+  ;;
+
+esac
+done
