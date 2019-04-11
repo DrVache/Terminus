e6d304916d3ed0128b4b12e59e048f1cbf2a5fad
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-04-11 17:52:09 +0200

    Update la_recherche_interminable.sh

--- TerminusQuest/Campus/Bethanie/A21/la_recherche_interminable.sh
+++ TerminusQuest/Campus/Bethanie/A21/la_recherche_interminable.sh
@@ -4,3 +4,32 @@
 echo "Retrouve ton groupe d’anglais grâce à ton nom.
 PS : Le fichier groupes_de_langue contient une centaine de nom avec le groupe
 correspondant à chaque étudiant."
+while [ true ]
+do
+	read cdutil
+case $cdutil in	
+	"ls") #Rajouter l option -l ?
+                ls
+                ;;
+		   "INVENTAIRE")  #avec "$" devant comme dans le cahier des charges, rien ne se passe
+                echo "Hé Jean2, ça va ?"
+                ;;	
+	  	 "jobs")
+               jobs
+                ;;
+				"cat groupes_de_langue | grep login")
+				echo "Voici ton groupe : "
+				cat groupes_de_langue | grep login
+				exit
+				;;
+	*)
+					
+					   if [ ${cdutil:0:2} = "cd" ]
+				        then
+					         cd ${cdutil:3}
+				        elif [ ${cdutil:0:3} = "cat" ]
+				        then
+							cat ${cdutil:4}
+						fi
+esac
+done
