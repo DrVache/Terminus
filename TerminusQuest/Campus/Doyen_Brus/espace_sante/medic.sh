95035071d96ca44886f33ff42124f4392420742c
FlorianGardes <fl.gardes@yahoo.fr>
2019-04-22 17:44:29 +0200

    Medic propre

--- TerminusQuest/Campus/Doyen_Brus/espace_sante/medic.sh
+++ TerminusQuest/Campus/Doyen_Brus/espace_sante/medic.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#echo "<Mercy> : Tapes la troisième lettre de mon prénom.
-#<Joueur> : R
-#<Mercy> : Tape la lettre manquante : INFORM_TIQUE
-#<Joueur> : A
-#<Mercy> : Tape la 19ème lettre de l’alphabet.
-#<Joueur> : S
-#<Mercy> : Tout va très bien, tu peux récupérer ton certificat
-#Si le joueur se trompe :
-#<Mercy> : Désolé, mais tu n’as pas l’air en forme, réessaie plus tard.
-#S’il réussie :
-#<Mercy> : Bravo, voici ton certificat !"
-
 echo "Bienvenue à l'infirmerie, nous allons voir comment tu te sens"
 echo "<Mercy> : Tapes la troisième lettre de mon prénom."
 
@@ -25,33 +13,33 @@ case $cdutils in
  ls
  ;;
 
-  "R")
-  echo "Tapes la lettre manquante : INFORM_TIQUE"
-  ;;
-  "r")
-  echo "Tapes la lettre manquante : INFORM_TIQUE"
-  ;;
+ *)
+  if [ "$cdutils" = "R" ] || [ "$cdutils" = "r" ]
+    then
+      echo "Tapes la lettre manquante : INFORM_TIQUE"
+      read cdutils
+      if [ "$cdutils" = "A" ] || [ "$cdutils" = "a" ]
+      then
+        echo "Tape la 19ème lettre de l’alphabet."
+        read cdutils
+        if [ "$cdutils" = "S" ] || [ "$cdutils" = "s" ]
+        then
+          echo "Bravo, voici ton certificat !"
+          exit
+        else
+          echo "Désolé, mais tu n’as pas l’air en forme, réessaie plus tard."
+          exit
+        fi
+      else
+        echo "Désolé, mais tu n’as pas l’air en forme, réessaie plus tard."
+        exit
+      fi
+    else
+      echo "Désolé, mais tu n’as pas l’air en forme, réessaie plus tard."
+      exit
+    fi
 
-  "A")
-  echo "Tape la 19ème lettre de l’alphabet."
-  ;;
-  "a")
-  echo "Tape la 19ème lettre de l’alphabet."
-  ;;
 
-  "S")
-  echo "Tout va très bien, tu peux récupérer ton certificat"
-  exit
-  ;;
-  "s")
-  echo "Tout va très bien, tu peux récupérer ton certificat"
-  exit
-  ;;
-
-  *)
-  echo "Désolé, mais tu n’as pas l’air en forme, réessaie plus tard."
-  exit
-  ;;
 
 esac
 done
