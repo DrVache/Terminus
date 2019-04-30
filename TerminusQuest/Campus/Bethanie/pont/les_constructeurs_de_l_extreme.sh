e27bfc06ba682c8b08e50f1e5ff2f4db35014573
SoloSquad <35772228+SoloSquad@users.noreply.github.com>
2019-04-30 17:06:21 +0200

    Update les_constructeurs_de_l_extreme.sh

--- TerminusQuest/Campus/Bethanie/pont/les_constructeurs_de_l_extreme.sh
+++ TerminusQuest/Campus/Bethanie/pont/les_constructeurs_de_l_extreme.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-echo "Répare le pont et traverse le à l’aide du mot de passe.
-Si on lance le script sans argument le message suivant est affiché : Ce script se
-lance avec un arguement, ajoute le mot de passe pour le lancer.
-Si le mot de passe est mauvais : "..."
-PS : Ne pas être sensible à la casse pour le mot de passe, le script téléporte
-l’utilisateur dans le répertoire /Campus/Arts_et_métiers/pont. En exécutant
-le script présent dans ce répertoire, on revient à /pont/. C’est un passage plus
-rapide et pour dynamiser la navigatio"
-
-
 if [ "$#" -ne 1 ]; then
- echo "Le nombre d'arguments est invalide"
-elif [ "$1" == "mellon" ]
+ echo "Il me faut le mot elfique en argument './script argument'"
+elif [ "$1" == "mellon" ] || [ "$1" == "Mellon" ]
 then 
 echo "Bravo c'est la bonne réponse"
-chmod 775 ../../Arts_et_Metiers
+chmod 700 ../../Arts_et_Metiers
 cd ../../Arts_et_Metiers
 exec $SHELL
+else 
+echo "Essaye encore !"
+exit
+
 fi
