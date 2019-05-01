2c8918a5af283e70f8a6fae969bf3b1fb96b4e5a
Diane Montiel <dmontiel@emi.u-bordeaux1.fr>
2019-05-01 11:32:42 +0200

    inventaire

--- Terminus.sh
+++ Terminus.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-mkdir Installation
-virtualenv butterfly>Installation/prérequis.txt 2>&1
+echo '||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+/!\ Attention des longueurs peuvent être observés. Patience! /!\
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'
+mkdir Installation>>prérequis.txt 2>&1
+mv prérequis.txt Installation/prérequis.txt
+virtualenv butterfly>>Installation/prérequis.txt 2>&1
 source butterfly/bin/activate
-pip install butterfly>>Installation/prérequis.txt
-pip install docker>>Installation/prérequis.txt
-butterfly.server.py --generate-certs --host=$(hostname)>Installation/certificat.txt
-butterfly.server.py --generate-user-pkcs=$(whoami)>>Installation/certificat.txt
-cp /net/cremi/$(whoami)/.config/butterfly/ssl/$(whoami).p12 /tmp>>Installation/certificat.txt
-chown $(whoami) /tmp/$(whoami).p12>>Installation/certificat.txt
-cp /net/cremi/$(whoami)/.config/butterfly/ssl/butterfly_ca.crt /tmp>>Installation/certificat.txt
+pip install butterfly>>Installation/prérequis.txt 2>&1
+machine=$(hostname)
+nom=$(whoami)
+butterfly.server.py --generate-certs --host=$machine>Installation/certificat.txt
+butterfly.server.py --generate-user-pkcs=$nom>>Installation/certificat.txt
+cp /net/cremi/$nom/.config/butterfly/ssl/$nom.p12 /tmp>>Installation/certificat.txt
+chown $nom /tmp/$nom.p12>>Installation/certificat.txt
+cp /net/cremi/$nom/.config/butterfly/ssl/butterfly_ca.crt /tmp>>Installation/certificat.txt
 echo '
 Dans les paramètres avancées du navigateur, importer le nouveau certificat dans vos paramétres HTTPS/SSL :
 /tmp/butterfly_ca.crt dans autorités
-/tmp/'$(whoami)'.p12 dans vos certificats.
+/tmp/'$nom'.p12 dans vos certificats.
 Appuyer sur Entrée quand vous avez terminer'
 google-chrome chrome://settings/certificates>Installation/ouverture_chrome.txt 2>&1 &
 read  inutile
-rm /tmp/$(whoami).p12
+rm /tmp/$nom.p12
 echo '<!DOCTYPE HTML>
 <html>
   <head>
@@ -34,7 +39,7 @@ echo '<!DOCTYPE HTML>
                   <iframe
                     height="85%"
                     width="460"
-                     src="https://'$(hostname)'.emi.u-bordeaux.fr:57575/">
+                     src="https://'$machine'.emi.u-bordeaux.fr:57575/">
                     </iframe>
                 </div>
                 <!--SECOND CADRE-->
@@ -47,6 +52,15 @@ echo '<!DOCTYPE HTML>
 </body>
 </html>
 '>index.html
-google-chrome https://$(hostname).emi.u-bordeaux.fr:57575/>>Installation/ouverture_chrome.txt 2>&1 &
-google-chrome index.html 2>&1 &
-butterfly.server.py --host=$(hostname) --port=57575 --login
+google-chrome https://$machine.emi.u-bordeaux.fr:57575/>>Installation/ouverture_chrome.txt 2>&1 &
+google-chrome index.html>>Installation/ouverture_chrome.txt 2>&1 &
+cd TerminusQuest/
+Quest=$(pwd)
+echo $Quest
+cp -r $Quest ~/
+cd
+echo -e 'export INVENTAIRE=~/TerminusQuest/Campus/Bethanie/A22/inventaire'
+echo -e 'alias Begin=\0047 cd TerminusQuest/Campus/Bethanie/ && cat Pr.Chen.txt\0047'>> ~/.bash_aliases
+source ~/.bashrc
+butterfly.server.py --host=$machine --port=57575 --login
+rm -r TerminusQuest
